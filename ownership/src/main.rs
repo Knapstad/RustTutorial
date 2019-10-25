afb1f1987225619b54fab30dd409c3bca783ac12
Bendik <knaben@OBOS.NO>
2019-10-25 14:46:43 +0200

    add mutable string

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,13 @@
-fn main() {//s is not valid here, not declared yet
+fn some_func() {//s is not valid here, not declared yet
 
     let s="hello";//s is declared and now valid
     //do stuff with s  here
 
-    }// s no loger valid, scope is ended
+    }// s no longer valid, scope is ended
+
+fn main(){
+
+    let mut s = String::from("Heisann"); //create mutable string
+    s.push_str(", verden!"); // appends to string
+    println!("{}",s);
+}

db3b1345d3b20a538b923da9700e9d68c4ee8fa9
Bendik <knaben@OBOS.NO>
2019-10-25 13:58:19 +0200

    add loop

--- controlflow/src/main.rs
+++ controlflow/src/main.rs
@@ -1,9 +1,18 @@
 fn main() {
-    let number = 3;
+    let number = 7;
 
     if number < 5{
         println!("condition was {}",number<5);
     } else {
         println!("condition was {}", number<5);
     }
+    let mut num = 1;
+    loop{
+        if num == 4{
+            break
+        }
+        println!("counting to 3... {}", num);
+        num+=1;
+    }
 }
+

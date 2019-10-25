45384d344cf6c367319ddaec370a492c0fc7695b
Bendik <knaben@OBOS.NO>
2019-10-25 15:00:06 +0200

    variable move

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -11,4 +11,17 @@ fn main(){
     let mut s = String::from("Heisann"); //create mutable string s now valid
     s.push_str(", verden!"); // appends to string
     println!("{}",s);
+
+    //ints
+    let x = 3; //bind 3 to x
+    let y = x; // copy 3 to y 
+    // ints are simple values with known fixed size, pushed to stack
+
+    //strings
+    let s1 = String::from("Hello"); // poiter pushed to stack values on heap
+    let s2= s1; // s1 moved to s2, s1 nolonger valid
+    println!("{}",s2); //s2 will work s1 not so mutch
+
+    // copy
+
 }// scope over and memory returned

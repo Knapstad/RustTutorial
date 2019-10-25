52ac5388624d317457d48097fa3953415172b10c
Bendik <knaben@OBOS.NO>
2019-10-25 09:43:59 +0200

    added main

---
+++ variables/target/debug/deps/variables-6bf002046e3c3b9c.d
@@ -0,0 +1,5 @@
+C:\Users\knaben\Documents\Scripts\Rust\tutorial\variables\target\debug\deps\variables-6bf002046e3c3b9c.exe: src\main.rs
+
+C:\Users\knaben\Documents\Scripts\Rust\tutorial\variables\target\debug\deps\variables-6bf002046e3c3b9c.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\knaben\Documents\Scripts\Rust\tutorial\variables\target\debug\variables.pdb: C:\Users\knaben\Documents\Scripts\Rust\tutorial\variables\src\main.rs

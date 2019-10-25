647ce91fed723091ecd03e6a9ac7869767cfecb9
Bendik <knaben@OBOS.NO>
2019-10-25 10:19:07 +0200

    remove toml and lockfile

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-   let x = 5;
-   println!("The value if x is: {}", x);
-   x = 6;
-   println!("The value if x is: {}", x);
+    let x = 5;
+    println!("The value if x is: {}", x);
+
+    let x = x + 1;
+
+    let x = x + 2;
+
+    println!("The value if x is: {}", x);
 }

174766eed8ba1edac1fbe8e8c7a420089efdfd3f
Bendik <knaben@OBOS.NO>
2019-10-25 09:02:51 +0200

    addeds random number

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng()
+                            .gen_range(1, 101);
+
+    println!("The secret number is {}", secret_number);
     println!("Please input your guess.");
 
     let mut guess = String::new();

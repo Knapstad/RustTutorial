1669d371619a85edc46edf03ba921f72bbc7b3fe
Bendik <knaben@OBOS.NO>
2019-10-25 13:16:55 +0200

    add more functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
     println!("Function test:");
-    another_function();
+    another_function(5);
+    yet_another_function("hello");
+
 }
-fn another_function() {
-    println!("Another Function!");
+fn yet_another_function(x: &str) {
+    println!("The value passed is {}", x);
+}
+
+fn another_function(x: i32) {
+    println!("The value passed is {}", x);
 }

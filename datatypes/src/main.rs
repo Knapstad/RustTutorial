f8a554818c7a03f8b0ffd3860f72f4416e5ca66e
Bendik <knaben@OBOS.NO>
2019-10-25 12:00:09 +0200

    added floats

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     let guess: u32 = "42".parse().expect("Not a number!");
+    let int = 42; //integer 32bit signed by default
+    let float = 4.2;// float default to 64bit
+
+
+    println!("{} {}",int, float)
+
+
 }

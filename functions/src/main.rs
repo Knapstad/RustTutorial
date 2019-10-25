cff2878619f95be864bc39968fe800e54f68542e
Bendik <knaben@OBOS.NO>
2019-10-25 13:19:01 +0200

    add multiparam funct

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     println!("Function test:");
     another_function(5);
     yet_another_function("hello");
+    multi_param_function(3, "Bendik");
 
 }
 fn yet_another_function(x: &str) {
@@ -10,4 +11,8 @@ fn yet_another_function(x: &str) {
 
 fn another_function(x: i32) {
     println!("The value passed is {}", x);
+}
+
+fn multi_param_function(x: i32, y: &str){
+    println!("The int passed is {}, the string is {}", x, y);
 }

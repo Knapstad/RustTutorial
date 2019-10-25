8e03794d71352a4c5fa8a7e8e5a8ac47993ea3cc
Bendik <knaben@OBOS.NO>
2019-10-25 15:26:50 +0200

    added ownership transferal

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,11 +1,3 @@
-//dead code
-fn some_func() {//s is not valid here, not declared yet
-
-    let s="hello";//s is declared and now valid
-    //do stuff with s  here
-
-    }// s no longer valid, scope is ended end dead code
-
 fn main(){
     
     let mut s = String::from("Heisann"); //create mutable string s now valid
@@ -28,7 +20,36 @@ fn main(){
     println!("{} {}",s2, s1); 
 
     let x = 5;
-    let y = x; //simple datatype, known size therfore this works
+    let y = x; //simple datatype, known size and are only stored on the stack, therfore this works
     println!("{} are the 'same' {}", x, y);
 
 }// scope over and memory returned
+
+fn some_func() {//s is not valid here, not declared yet
+
+    let s="hello";//s is declared and now valid
+    //do stuff with s  here
+
+    }// s no longer valid, scope is ended end dead code
+
+fn ownership_transferal(){
+
+    let s1 = gives_ownership();//gives ownership moves its returnvalue
+                               //to s1
+
+    let s2 = String::from("Hello");// s2 enters scope
+
+    let s3 = takes_and_gives_back(s2); // s2 is moved in to takes_and_gives_back
+                                      // move return in to s3
+} // s3 goes out of scope as does s1 and both are dropped
+
+fn gives_ownership() -> String{
+    let some_string = String::from("Hello");
+
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String{ //a_string comes in to scope
+
+    a_string  //a_string is returned
+}

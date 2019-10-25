d27941bec0cfbf91f4379cb36a4055581cb9b8ef
Bendik <knaben@OBOS.NO>
2019-10-25 12:05:14 +0200

    added numeric opperators

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -4,7 +4,16 @@ fn main() {
     let float = 4.2;// float default to 64bit
 
 
-    println!("{} {}",int, float)
+    println!("{} {}",int, float);
 
+    let sum = 5 + 10;
 
+    let difference = 95.5 - 3.1;
+
+    let product = 4 * 30;
+
+    let quotient = 56.7/32.1;
+
+    let remainder = 43 % 5;
+    println!("sum: {}\ndiff: {}\nprod: {}\nquot: {}\nremain: {}", sum, difference, product, quotient, remainder)
 }

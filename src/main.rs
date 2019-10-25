a69ceff764cd2306f017ccf86bbff0c27df94e64
Bendik <knaben@OBOS.NO>
2019-10-25 09:15:49 +0200

    moved loop

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
+    
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng()
+                            .gen_range(1, 101);
+
+    println!("The secret number is {}", secret_number);
     loop{
-        println!("Guess the number!");
-
-        let secret_number = rand::thread_rng()
-                                .gen_range(1, 101);
-
-        println!("The secret number is {}", secret_number);
         println!("Please input your guess.");
 
         let mut guess = String::new();

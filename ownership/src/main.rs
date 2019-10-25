81593e8cfbc07027015fd9f0af879fd45063d110
Bendik <knaben@OBOS.NO>
2019-10-25 14:49:53 +0200

    memory return

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,13 +1,14 @@
+//dead code
 fn some_func() {//s is not valid here, not declared yet
 
     let s="hello";//s is declared and now valid
     //do stuff with s  here
 
-    }// s no longer valid, scope is ended
+    }// s no longer valid, scope is ended end dead code
 
 fn main(){
-
-    let mut s = String::from("Heisann"); //create mutable string
+    
+    let mut s = String::from("Heisann"); //create mutable string s now valid
     s.push_str(", verden!"); // appends to string
     println!("{}",s);
-}
+}// scope over and memory returned
